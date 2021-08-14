acbac261bee2b2671a1ff6bdac8d218928708b3f
JJ-Dev18 <jj.murillo182020@gmail.com>
2021-08-13 20:11:11 -0500

    segundo intento subir bd

--- db.php
+++ db.php
@@ -1,33 +1,38 @@
 <?php
-   $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+//  
   class DB{
-     
+    private $dbUrl; 
     private $host;
     private $user ;
     private $password ;
     private $db ;
-    private $active_group = 'default';
-    private $query_builder = TRUE;
 
     public function __construct(){
-        $this->host     = $cleardb_url["host"];;
-        $this->db       = substr($cleardb_url["path"],1);
-        $this->user     = $cleardb_url["user"];
-        $this->password = $cleardb_url["pass"];
+        // $this->host     = $cleardb_url["host"];;
+        // $this->db       = substr($cleardb_url["path"],1);
+        // $this->user     = $cleardb_url["user"];
+        // $this->password = $cleardb_url["pass"];
+        // $this->dbUrl = parse_url(getenv("CLEARDB_DATABASE_URL"));
         $this->charset  = 'utf8mb4';
     }
 
     function connect(){
     
         try{
+            $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+            $cleardb_server = $cleardb_url["host"];
+            $cleardb_username = $cleardb_url["user"];
+            $cleardb_password = $cleardb_url["pass"];
+            $cleardb_db = substr($cleardb_url["path"],1);
             
-            $connection = "mysql:host=" . $this->host . ";dbname=" . $this->db . ";charset=" . $this->charset;
+            $connection = "mysql:host=" . $cleardb_server . ";dbname=" . $cleardb_db . ";charset=" . $this->charset;
             $options = [
                 PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_EMULATE_PREPARES   => false,
             ];
-            $pdo = new PDO($connection, $this->user, $this->password, $options);
-           
+            $pdo = new PDO($connection, $cleardb_username, $cleardb_password, $options);
+            $active_group = 'default';
+            $query_builder = TRUE;
             return $pdo;
 
         }catch(PDOException $e){
